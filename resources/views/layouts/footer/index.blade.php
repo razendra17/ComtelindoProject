e4f52c585b0c194cdc24973b055827a902851411
Cyoku-ell <kokuofficial1@gmail.com>
2026-02-25 12:14:50 +0800

    tai

--- resources/views/layouts/footer/index.blade.php
+++ resources/views/layouts/footer/index.blade.php
@@ -1,5 +1,5 @@
-<footer class=" bottom-0 left-0 right-0 bg-[rgb(255,133,33)] p-4  flex justify-center">
-    <div class="text-center text-white mb-5 text-sm opacity-70">
+<footer class=" bottom-0 left-0 right-0 bg-[rgb(255,133,33)] flex justify-center">
+    <div class="text-center text-white  bg-center mb-5 text-sm opacity-70">
         © {{ date('Y') }} Comtelindo. All rights reserved.
     </div>
 </footer>
