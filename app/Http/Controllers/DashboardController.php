8edab1cf040a3c6947768c4086f7ccb75d5137a9
razendra17 <daffaevandrez25@gmail.com>
2026-02-26 14:19:16 +0800

    sidebar fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,29 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Constant;
 use App\Http\Controllers\Controller;
 use App\Models\Data;
+use App\Models\Package;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('pages.dashboard.index');
+    public function index()
+    {
+        $data = Package::all();
+        $alldata = $data->count();
+        $approved = Package::where('status', Constant::status['approved'])->count();
+        $rejected = Package::where('status', Constant::status['rejected'])->count();
+        $pending  = Package::where('status', Constant::status['pending'])->count();
+        
+        // return response()->json([
+        //     $alldata,
+        //     $approved,
+        //     $rejected,
+        //     $pending
+        // ]);
+        return view('pages.admin.dashboard.index', [
+            'data' => $alldata,
+            'approved' => $approved,
+            'rejected' => $rejected,
+            'pending' => $pending
+        ]);
     }
     public function data()
     {
 
-            $query = Data::with('package.city');
-            return DataTables::of($query)
-                ->addColumn('package_name', function ($row) {
-                    return $row->package?->name;
-                })
-                ->addColumn('city_name', function ($row) {
-                    return $row->package?->city?->name;
-                })
-                ->make(true);
-
+        $query = Data::with('package.city');
+        return DataTables::of($query)
+            ->addColumn('package_name', function ($row) {
+                return $row->package?->name;
+            })
+            ->addColumn('city_name', function ($row) {
+                return $row->package?->city?->name;
+            })
+            ->make(true);
     }
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
 <body class="min-h-screen flex flex-col">
